f688e7a6d90acafe264f9e2271c2b068e2b27c14
Çağdaş ÖZER <cagdas.1997@hotmail.com>
2024-02-13 20:25:13 +0300

    Day2 Lesson 1

--- Program.cs
+++ Program.cs
@@ -36,3 +36,59 @@ for (int i = 0; i < courses2.Length; i++)
 {
     Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price + "TL");
 }
+
+
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.Id = 1;
+customer1.NationalIdentity = "12345678901";
+customer1.FirstName = "Çağdaş";
+customer1.LastName = "ÖZER";
+customer1.CustomerNumber = "123456";
+
+IndividualCustomer customer2 = new IndividualCustomer();
+
+customer2.Id = 2;
+customer2.NationalIdentity = "12345678902";
+customer2.FirstName = "Ulaş";
+customer2.LastName = "ÖZER";
+customer2.CustomerNumber = "123457";
+
+CorporateCustomer customer3 = new CorporateCustomer();
+customer3.Id = 3;
+customer3.Name = "Kodlamaİo";
+customer3.CustomerNumber = "123789";
+customer3.TaxNumber = "1234567890";
+
+CorporateCustomer customer4 = new CorporateCustomer();
+customer4.Id = 4;
+customer4.Name = "ÖzerRent";
+customer4.CustomerNumber = "123987";
+customer4.TaxNumber = "1234567891";
+
+
+//int n1 = 10;
+//int n2 = 20;
+//n1 = n2;
+//n2 = 50;
+//Console.WriteLine(n1);
+
+//value types --> int, bool, double...
+
+//string[] cities1 = { "Ankara", "İstanbul", "İzmir" };
+//string[] cities2 = { "Bursa", "Erzurum", "Erzincan" };
+
+//cities2 = cities1;
+//cities1[0] = "Adana";
+
+//Console.WriteLine(cities2[0]);
+
+//referance types --> array, class, interface...
+
+                              //101        //102      //103       //104
+BaseCustomer[] customers = { customer1, customer2, customer3, customer4 };
+
+//polymorphism
+foreach (BaseCustomer customer in customers)
+{
+    Console.WriteLine(customer.CustomerNumber);
+}
